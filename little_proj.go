77ad7a46184bea85f7322e99e8e747043403c4a2
Anatoly Pozdnyshev <pozdnyshev.anatol@gmail.com>
2024-02-12 17:17:14 +0300

    new func

--- little_proj.go
+++ little_proj.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
-func average(numbers []float64) float64 {
-    total := 0.0
-    for _, num := range numbers {
-        total += num
+func sieveOfEratosthenes(n int) []int {
+    prime := make([]bool, n+1)
+    for i := 2; i <= n; i++ {
+        prime[i] = true
     }
-    return total / float64(len(numbers))
+
+    for p := 2; p*p <= n; p++ {
+        if prime[p] == true {
+            for i := p * p; i <= n; i += p {
+                prime[i] = false
+            }
+        }
+    }
+
+    var primes []int
+    for p := 2; p <= n; p++ {
+        if prime[p] == true {
+            primes = append(primes, p)
+        }
+    }
+    return primes
 }
 
 func main() {
-    numbers := []float64{3.5, 6.2, 8.9, 10.1, 4.8}
-    avg := average(numbers)
-    fmt.Println("Среднее значение:", avg)
+    n := 50
+    primes := sieveOfEratosthenes(n)
+    fmt.Println("Простые числа до", n, ":", primes)
 }
